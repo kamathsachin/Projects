c2d2418fe725e8351ec61b51660b16e34259d629
Sachin Kamath <kamathsachin@hotmail.com>
2021-02-07 14:21:43 -0600

    Created card component

---
+++ Robofriends/robofriends/src/Robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Sachin Kamath",
+    username: "SacKam",
+    email: "sackam@email.com",
+  },
+  {
+    id: 2,
+    name: "Shubha Kamath",
+    username: "ShuKam",
+    email: "shukam@email.com",
+  },
+  {
+    id: 3,
+    name: "Shaurya Kamath",
+    username: "ShaKam",
+    email: "shakam@email.com",
+  },
+  {
+    id: 4,
+    name: "Seema Kamath",
+    username: "SeeKam",
+    email: "seekam@email.com",
+  },
+  {
+    id: 5,
+    name: "Shreya Kamath",
+    username: "ShrKam",
+    email: "shrkam@email.com",
+  },
+  {
+    id: 6,
+    name: "Sheela Kamath",
+    username: "SheKam",
+    email: "shekam@email.com",
+  },
+  {
+    id: 7,
+    name: "Jayawanth Kamath",
+    username: "JayKam",
+    email: "jaykam@email.com",
+  },
+  {
+    id: 8,
+    name: "Srinivasa Kamath",
+    username: "SriKam",
+    email: "srikam@email.com",
+  },
+];
